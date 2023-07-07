a7e5ba2a1ea5c544b98a950699f03f33ae319883
Henok Mekonnen <henatan99@yahoo.com>
2023-07-07 10:29:30 -0400

    header search input and responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,30 +4,40 @@ import Profile from "../profile.jpg";
 import { AiOutlineMessage } from 'react-icons/ai';
 import { MdExpandMore } from 'react-icons/md';
 import { IoMdNotificationsOutline } from 'react-icons/io';
+import {CiSearch} from 'react-icons/ci';
 
 const Header = () => {
     return (
-        <header className="flex flex-row w-full px-[12%] py-3 bg-custom-blue">
+        <header className="flex flex-row items-center w-full px-[12%] py-3 bg-custom-blue">
             <div className="w-28 flex flex-row items-center">
                 <img src={LogoWhite} alt="logo"></img>
             </div>
-            <div></div>
-            <nav className="ml-auto">
-                <ul className="flex flex-row text-white justify-between items-center w-48">
+            <div className="relative ml-[10%] w-[40%] hidden md:flex items-center">
+                <input 
+                    type="text"
+                    placeholder="Enter interests, keyword, company name, etc."
+                    className="leading-tight pl-2 pr-6 py-1 rounded-md border border-gray-300 w-full focus-outline-none"
+                />
+                <button className="absolute right-0 mr-2">
+                    <CiSearch />
+                </button>
+            </div>
+            <nav className="ml-auto hidden md:block">
+                <ul className="flex flex-row text-white justify-between items-center w-40">
                     <li className="flex flex-row items-center">
                         <button className="">
-                            <AiOutlineMessage size={16}/>
+                            <AiOutlineMessage size={14}/>
                         </button>
                     </li>
                     <li className="flex flex-row items-center">
-                        <span className="w-8">EN</span>
+                        <span className="w-6">EN</span>
                         <button>
                             <MdExpandMore size={24}/>
                         </button>  
                     </li>
                     <li className="flex flex-row items-center">
                         <button>
-                            <IoMdNotificationsOutline size={18}/>
+                            <IoMdNotificationsOutline size={16}/>
                         </button>
                     </li>
                     <li className="flex flex-row items-center">

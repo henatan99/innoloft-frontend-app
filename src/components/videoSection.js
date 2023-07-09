7a307d5c2e9ead9089e9c923996de7e9f7a87521
Henok Mekonnen <henatan99@yahoo.com>
2023-07-09 13:20:26 -0400

    OfferDetails

--- src/components/videoSection.js
+++ src/components/videoSection.js
@@ -1,3 +1,4 @@
+import './video.css';
 import React from "react";
 import ReactPlayer from "react-player";
 
@@ -10,21 +11,9 @@ const VideoSection = (props) => {
             <h2 className="text-left py-2">Video</h2>
             {
                 type === 'edit' ?
-
-                // <iframe 
-                //     className="min-w-[60%] max-w-full mx-auto" 
-                //     src={videoUrl}
-                //     title="YouTube Video"
-                //     frameBorder="0"
-                //     allow="autoplay; encrypted-media"
-                //     allowFullScreen
-                // >
-                //     {/* <source src={videoUrl} type="video/mp4" /> */}
-                //     {/* Add additional source elements for different video formats */}
-                //     {/* Your browser does not support the video tag. */}
-                // </iframe> 
-                <div className="mx-auto min-w-[60%] max-w-full">
-                    <ReactPlayer url={videoUrl} style={{margin: 'auto'}}/>
+ 
+                <div className="video mx-auto min-w-[60%] max-w-full w-auto">
+                    <ReactPlayer url={videoUrl} className="player" controls/>
                 </div>
                 
                 :
